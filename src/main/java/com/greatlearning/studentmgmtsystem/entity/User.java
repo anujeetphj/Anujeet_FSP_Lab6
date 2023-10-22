a80084b2c2d982bb34917e02497fa13f126adfc6
anujeetphj <anujeetphj@gmail.com>
2023-10-22 23:57:37 +0530

    Add files via upload

---
+++ src/main/java/com/greatlearning/studentmgmtsystem/entity/User.java
@@ -0,0 +1,71 @@
+package com.greatlearning.studentmgmtsystem.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+   @Id
+   @Column(name = "user_id")
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(name="username")
+   private String username;
+   @Column(name="password")
+   private String password;
+    
+   @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+   @JoinTable(
+           name = "users_roles",
+           joinColumns = @JoinColumn(name = "user_id"),
+           inverseJoinColumns = @JoinColumn(name = "role_id")
+           )
+   private List<Role> roles = new ArrayList<>();
+
+	public List<Role> getRoles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return username;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+}
+
---
+++ src/main/java/com/greatlearning/studentmgmtsystem/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.greatlearning.studentmgmtsystem.service;
+
+import java.util.List;
+
+import com.greatlearning.studentmgmtsystem.entity.Student;
+
+
+public interface StudentService {
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student thestudent);
+
+	public void deleteById(int theId);
+
+	
+
+}
